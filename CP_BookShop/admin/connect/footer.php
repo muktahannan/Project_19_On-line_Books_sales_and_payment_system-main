1a14084a987cc0fa7fee020621a3336590486195
Fatema Akter <49169091+muktahannan@users.noreply.github.com>
2023-06-19 00:24:36 +0100

    Delete CP_BookShop directory

--- CP_BookShop/admin/connect/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<footer><br>
-<center><p>copyright &copy; by CP</p></center></footer>
-		</div>
-		
-	</body>
-</html>
--- CP_BookShop/connect/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-
-<div id="footer">Copyright Fatema Akter-@-2016
-
- </div>
-
-</div>
-</body></html>
